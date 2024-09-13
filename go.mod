c160114f008af3d1cc98bda3d71c236c74e1ef8b
Kolton Starr <carnegieroyce@protonmail.com>
2024-09-13 18:10:28 -0400

    done

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module hashmaster
 
 go 1.23.1
+
+require (
+	github.com/AlecAivazis/survey/v2 v2.3.7 // indirect
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/term v0.24.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,3 +1,85 @@
 package main
 
-func main() {}
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+	"hash"
+
+	"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/blake2s"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/fatih/color"
+)
+
+
+func main() {
+	// Survey for input
+    hashAlgorithms := []string{
+		"MD5", 
+		"SHA-256", 
+		"SHA-512",
+		"BLAKE2b", 
+		"BLAKE2s", 
+		"HMAC",
+	}
+
+	color.HiBlue("********** Welcome to the Hash Master! **********")
+
+    algorithmPrompt := &survey.Select{
+		Message: "Choose a hashing algorithm:",
+        Options: hashAlgorithms,
+    }
+	
+	var selectedOption string
+    err := survey.AskOne(algorithmPrompt, &selectedOption)
+
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+
+	messagePrompt := &survey.Input{
+		Message: "Enter the text to hash:",
+	}
+
+	var message string
+	survey.AskOne(messagePrompt, &message)
+
+	var hasher hash.Hash
+
+    switch selectedOption {
+	case "MD5":
+		hasher = md5.New()
+    case "SHA-256":
+        hasher = sha256.New()
+    case "SHA-512":
+        hasher = sha512.New()
+    case "BLAKE2b":
+        hasher, err = blake2b.New256(nil)
+        if err != nil {
+            fmt.Println("Error:", err)
+            return
+        }
+    case "BLAKE2s":
+        hasher, err = blake2s.New256(nil)
+        if err != nil {
+            fmt.Println("Error:", err)
+            return
+        }
+    case "HMAC":
+        hasher = hmac.New(sha256.New, []byte("secret"))
+    default:
+        fmt.Println("Unknown algorithm selected")
+        return
+    }
+
+    hasher.Write([]byte(message))
+    hash := hasher.Sum(nil)
+	color.White("Hash: %x\n", hash)
+
+	
+}
